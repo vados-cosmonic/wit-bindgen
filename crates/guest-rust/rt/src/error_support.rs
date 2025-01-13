e368d7972e1b9ef03f2e435c0983f28d317e5a89
Victor Adossi <vadossi@cosmonic.com>
2025-01-13 22:58:00 +0900

    feat: add error-context-new
    This commit adds a helper for for claling `error-context.new`
    
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/guest-rust/rt/src/error_support.rs
+++ crates/guest-rust/rt/src/error_support.rs
@@ -1,6 +1,7 @@
 #![deny(missing_docs)]
 
 use std::fmt::{self, Debug, Display};
+use std::string::String;
 
 /// Represents the Component Model `error-context` type.
 pub struct ErrorContext {
@@ -50,6 +51,30 @@ impl Drop for ErrorContext {
             if self.handle != 0 {
                 unsafe { error_drop(self.handle) }
             }
+        }
+    }
+}
+
+/// Call the `error-context.new` canonical built-in function.
+pub fn error_context_new(debug_message: &str) -> ErrorContext {
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        _ = debug_message;
+        unreachable!();
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        #[link(wasm_import_module = "$root")]
+        extern "C" {
+            #[link_name = "[error-context-new]"]
+            fn error_context_new(_: i32, _: i32) -> i32;
+        }
+
+        let len = i32::try_from(debug_message.len()).expect("invalid debug message length");
+        unsafe {
+            let err_ctx_handle = error_context_new(debug_message.as_ptr(), len);
+            ErrorContext::from_handle(u32::try_from(err_ctx_handle))
         }
     }
 }
