b253766f95e0b7c1d890e21fb05d19a98c68655a
Victor Adossi <vadossi@cosmonic.com>
2025-07-29 18:58:01 +0900

    chore: reset testing changes

--- tests/runtime-async/async/cancel-import/runner.c
+++ tests/runtime-async/async/cancel-import/runner.c
@@ -12,35 +12,23 @@ int main() {
     test_future_void_writer_t writer;
     test_future_void_t reader = test_future_void_new(&writer);
 
-    runner_callback_code_t cc = test_async_pending_import(reader);
-    fprintf(stderr, "CALLBACK CODE (post-pending-import-call) [%d]\n", cc);
-    fflush(stderr);
-    // TODO figure out what this should be... should it immediately yield?
-    //assert(cc == 2); // TODO: we probably need to separate out the direct comparison and set macro?
-    // TODO: *undo* the bit shift
+    /* runner_subtask_status_t status = test_async_pending_import(reader); */
+    /* assert(RUNNER_SUBTASK_STATE(status) == RUNNER_SUBTASK_STARTED); */
 
-    runner_subtask_t subtask = RUNNER_SUBTASK_HANDLE(cc);
+    runner_subtask_status_t status = test_async_pending_import(reader);
+    assert(RUNNER_SUBTASK_STATE(status) == RUNNER_SUBTASK_STARTED);
+
+    runner_subtask_t subtask = RUNNER_SUBTASK_HANDLE(status);
     assert(subtask != 0);
 
-    runner_subtask_status_t status = runner_subtask_cancel(subtask);
-    // Buggy line is below
-    //
-    // This is returning <???> *instead* of SUBTASK_RETURNED_CANCELLED
-    fprintf(stderr, "SUBTASK STATE (pre-macro) [%d]\n", status);
-    fprintf(stderr, "SUBTASK STATE (poste-macro) [%d]\n", RUNNER_SUBTASK_STATE(status));
-    fflush(stderr);
+    status = runner_subtask_cancel(subtask);
     assert(RUNNER_SUBTASK_STATE(status) == RUNNER_SUBTASK_RETURNED_CANCELLED);
-    fprintf(stderr, "subtask state is correct");
-
     assert(RUNNER_SUBTASK_HANDLE(status) == 0);
-    fprintf(stderr, "subtask handle status is correct");
 
     runner_waitable_status_t status2 = test_future_void_write(writer);
-    fprintf(stderr, "void write worked");
     assert(RUNNER_WAITABLE_STATE(status2) == RUNNER_WAITABLE_DROPPED);
     assert(RUNNER_WAITABLE_COUNT(status2) == 0);
     test_future_void_drop_writable(writer);
-    fprintf(stderr, "everything worked");
   }
 
   /* // One import in "started", one in "starting", then cancel both. */
